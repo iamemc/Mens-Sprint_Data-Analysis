4548ebdfb510b64492e1fab49df3251d2ea608ef
iamemc <38144884+iamemc@users.noreply.github.com>
2021-02-12 20:20:34 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,180 @@
-# Mens-Sprint_Data-Analysis
+## METADATA
+
+- Ana Castanheira
+	analuciacastanhei@gmail.com
+
+- Eduardo Carvalho
+	eduardo_martins_carvalho@hotmail.com
+
+- Introduction to Programming.
+	-- Lecturer: Dr. José Campos.
+
+- FCUL - Faculdade de Ciências da Universidade de Lisboa.
+
+DELIVERY STRUCTURE
+---------------
+- README.md
+- main.py
+- csv_files
+    - athletes.csv
+	- sport_events.csv
+- images [if created]
+
+INTRODUCTION
+---------------
+
+> This assignment was made by Eduardo Carvalho and Ana Castanheira for the Introduction to 
+> Programming class - lectured by Dr. José Campos.
+> Within, are 2 csv structured files representing Cycling Olympic Players and selected data of 
+> their performance at the Men's Sprint events in one or many Olympic Editions.
+> The PDF file contains a data analysis of the athlete statistics per edition.
+
+REQUIREMENTS
+---------------
+None. As it's only a database as of now, a plain text editor or excel-type software will work.
+
+> We recommend opening this readme.md file in a markdown editor/interface for proper displaying.
+
+Recommended:
+```sh 
+Excel 2016 or later
+Notepad++
+```
+
+Men's Sprint Rules
+---------------
+> *As defined by Union Cycliste Internationale (UCI) rules, the first round of competition used 
+> to qualify for the sprint competition is the flying 200 m time trial. In this round each rider 
+> completes two to three warm up laps and then completes the final 200 m, which is usually just 
+> under a lap. The number of riders that qualify for the sprint rounds depends on the competition;
+> in World Cup competitions, 16 riders will advance and in a world championship, 24 riders will advance. 
+> The top riders are seeded in the following rounds, meaning the fastest qualifier will face the slowest 
+> qualifier and so on. Knock-out rounds then proceed, initially on a one race basis and then on a best-of 
+> three-race format from the quarter-final stage. Riders defeated in the earlier rounds may get a chance to 
+> continue in the competition through the repechage races.*
+
+### Olympic's Specific Rules
+> *Matches are contested between two riders who cover three laps of the track. The first rider over the line 
+> wins the race, best of three races wins the match. The top 16 riders qualify for the knockout stages with 
+> a flying 200-metre time trial.*
+
+ATHLETES.CSV STRUCTURE
+---------------
+
+|athete_id|name                         |date_of_birth|nation             |sex |
+|---------|-----------------------------|-------------|-------------------|----|
+|1        |Jason Kenny                  |23/03/1988   |Great Britain      |male|
+|2        |Callum Skinner               |20/08/1992   |Great Britain      |male|
+|3        |Matthew Glaetzer             |24/08/1992   |Australia          |male|
+|4        |Denis Dmitriev               |23/03/1986   |Russia             |male|
+|5        |Grégory Baugé                |31/01/1985   |France             |male|
+
+Table Caption
+
+- **athlete_id**: Identifier number of each individual athlete_id
+	- **Type: Int**
+    	- Positive values(1 till infinite)
+- **name**: Athele's name
+	- **Type - String**
+- **date_of_birth**: day/month/year of birth of each athlete
+	- **Type - String**
+- **nation**: Country that each Athlete represented while sporting. May not reflect country of birth.
+	- **Type - String**
+- **sex**: Gender of each athlete
+	- **Type - String** 
+		- Two possible "values": **female** or **male**
+
+
+ATHLETES.CSV STRUCTURE
+---------------
+
+|year|athete_id|race_id|round                    |heat|time   |avg_km_h|rank|result|record|host_country  |venue                   |
+|----|---------|-------|-------------------------|----|-------|--------|----|------|------|--------------|------------------------|
+|2016|1        |101    |qualifying               |1   |9,551  |75,384  |1   |Q     |OR    |Brazil        |Rio Olympic Velodrome   |
+|2016|2        |103    |qualifying               |1   |9,703  |74,203  |2   |Q     |N     |Brazil        |Rio Olympic Velodrome   |
+|2016|3        |72     |qualifying               |1   |9,704  |74,196  |3   |Q     |N     |Brazil        |Rio Olympic Velodrome   |
+|2016|4        |143    |qualifying               |1   |9,774  |73,664  |4   |Q     |N     |Brazil        |Rio Olympic Velodrome   |
+|2016|5        |94     |qualifying               |1   |9,807  |73,416  |5   |Q     |N     |Brazil        |Rio Olympic Velodrome   |
+
+**Table Caption**
+
+ - **year**: *Date of when the event happened* 
+	 - **Type - Int** 
+	 	 - Positive number with four digits;
+		 - (Years considered: 2016, 2012, 2008, 2004, 2000, 1896)
+ - **athlete_id**: *Identifier number of each individual athlete_id*
+	 - **Type - Int**
+	    - Positive number with possible values of 1 till infinite;
+ - **race_id**: *Individual Identifier of an Athlete in a race (i.e. Shirt Numbering) - NA (Not Appliable) if no Personal Numbering was used*. 
+	- **Type -  Int**
+	    - Positive number with possible values of 1 till infinite;
+- **round**: *Round of the event competition - i.e. Qualifyings - 1/16, 1/8, Quarter Finals... etc.*
+	- **Type - String**
+- **heat**: *Which race each round was, each round may have several heats.* ; **Type - Int**
+	    - Positive number with possible values of 1 till infinite;
+- **time**: *How long, in seconds, each competitor took to finish the race (less is better)*
+	- **Type - Float**
+	    - Positive values with 3 decimals; this variable can take no value due to athlete quiting (-)
+- **avg_km_h**: *Average speed per Kilometer of each competitor (more is better)* 
+	- **Type - Float**
+    	- This variable can take no value due to athlete quiting (-)
+- **rank**: *Place each competitor finished each race.* 
+	- **Type - Int**
+    	- Positive number with possible value in range 1 to infinite
+- **result**: *What happened to each competitor after each round*
+	- **Type - String**
+    	- **Q** - *Qualified to next round*
+    	- **N** - *Didn't Qualify to next round*
+    	- **R** - *Repechaged - Didn't Automatically qualified but stille given a chance to win a spot in the following round*
+    	- **TBD** - *To Be Decided, in case it's a best of 3/5 and more races between the same competitors are yet to take place.*
+    	- **B** - *Qualified to the Bronze medal race.*
+    	- **C** - *Qualified to a placement in the final table race excluding 1-4th. I.e. race to 9-12th placement, 5th-8th placement.*
+    	- **Number** *(i.e. 1,2,3,4,5,6...) - Final athlete placement in the competition.*
+    	- **DNS** - *Did Not Start, disqualified due to not starting the race*
+- **record** - *If a record happened in each race, OR - Olympic Record, WR - World Record.*
+	- **Type - String**
+- **host_country**: *Where the competition took place, in this case, Olympic Games organizer.*
+	- **Type - String**
+- **venue**: *In what velodrome each race took place*.
+	- **Type - String**
+
+Data Limitations
+---------------
+> While data collecting we experienced some trouble finding some needed data.
+Except for 2016, there wasn´t any personal identifier used (i.e. shirt numbering) 
+which in 16 is equivalente to **race_id**. Also, some of the time difference 
+(**time**) of the athletes that did not make the first place is missing as well. 
+So we just replaced the data we did not have by "**NA**" - Not Applicable.
+
+> Curiously, we added the information of the winner of the very first man´s cycling sprint event 
+at the 1896 Olympic Games that took place in Greece, for future comparison for assignment 2.2, as baseline data.
+
+Resources Consulted
+---------------
+https://www.olympedia.org/editions/59
+
+**2016:**
+https://en.wikipedia.org/wiki/Cycling_at_the_2016_Summer_Olympics_%E2%80%93_Men%27s_sprint
+https://www.olympic.org/rio-2016/cycling-road
+https://www.cyclingweekly.com/news/racing/olympics/rio-2016-olympic-games-cycling-medal-table-272364
+www.assetrio2016.azureedge.net/_odf-documents
+https://web.archive.org/web/20160806081759/https://www.rio2016.com/en/cycling-track-standings-ct-mens-sprint
+
+**2012:**
+https://en.wikipedia.org/wiki/Cycling_at_the_2012_Summer_Olympics_%E2%80%93_Men%27s_sprint
+
+**2008:**
+https://en.wikipedia.org/wiki/Cycling_at_the_2008_Summer_Olympics_%E2%80%93_Men%27s_sprint
+
+**2004:**
+https://en.wikipedia.org/wiki/Cycling_at_the_2004_Summer_Olympics_%E2%80%93_Men%27s_sprint
+
+**1896:**
+https://www.olympedia.org/results/153001
+
+MANTAINERS
+---------------
+
+This program was coded and is maintained by:
+> Ana Castanheira | analuciacastanhei@gmail.com | Master's in Biostatistics | FCUL
+> Eduardo Carvalho | eduardo_martins_carvalho@hotmail.com | Master's in Informatics | FCUL
---
+++ main.py
@@ -0,0 +1,388 @@
+#https://docs.google.com/document/d/1Y31Nt05peNIPwLo9O_TsTRAcy0GbDqGvRcHvJ4qtgzk/edit
+###################### Indíce ####################### 
+#---- Instruções
+#---- Imports
+#---- Funções
+#---- Leitura de Ficheiros
+#---- Correcções à Base de Dados
+#---- Tabelas Descritivas
+#     ---- Nrº Ciclistas por País
+#     ---- Média, Moda, Mediana, etc
+#     ---- Nome dos Vencedores por Edição
+#---- Gráficos
+#     ---- Ciclistas por Continente
+#     ---- Média de Tempo por Ronda e Edição
+#     ---- Média de km/H por Edição
+#     ---- Média de Idade por Edição
+#     ---- Média de Tempo por Corrida 
+#---- Testes
+#     ---- Tempo para correr código
+#===============================================================
+#======================== Instruções ===========================
+#===============================================================
+""" CORRER NA CONSOLA: pip install dataframe_image """
+
+#================= Erros em versões antigas =====================
+"""# Garantir que se usa a última versões do Pandas:
+# Pandas updated to 1.2.0 or above
+
+# Pandas <= 1.2.0, origina: 
+#   'DataFrame' object has no attribute 'to_frame'
+# Corrigido com:
+# XYZ = XYZ.to_frame()
+# XYZ = XYZ.reset_index()
+
+#https://pandas.pydata.org/docs/whatsnew/v1.2.0.html"""
+
+#===============================================================
+#========================= Imports =============================
+#===============================================================
+import matplotlib.pyplot as plt
+import dataframe_image as dfi
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import unittest
+import time
+import os
+start = time.time()
+
+#===============================================================
+#========================= Funções =============================
+#===============================================================
+#================= Validate Image Directory =====================
+def directoryValidator():
+    if os.path.isdir("./images/") == False: #check if directory exists
+        os.makedirs(os.path.dirname("./images/")) #create directory
+    else:
+        pass #if it exists, all good.
+
+#================= Image Saver =====================
+image_counter = 0
+def saveImages(category, graph_name, variable):
+    global image_counter, table_counter 
+    directoryValidator()
+    image_counter += 1
+    if (category) == "graph":
+        (graph_name).savefig("./images/{}_{}.png".format(str(category),str(image_counter)))
+        print("\n\t Graph image saved to 'images' folder as {}_{}.png".format(str(category),str(image_counter)))
+    elif (category) == "table":
+        (graph_name).export((variable), "./images/{}_{}.png".format(str(category),str(image_counter)))
+        print("\n\t Table image saved to 'images' folder as {}_{}.png".format(str(category),str(image_counter)))
+    else:
+        print("\n No valid image Category found.")
+
+#================= Athlete Age =====================
+def obterIdade(nome,pais_jogo): #pais_jogo->host_country
+    athlete_id = 0
+    idade = None
+    contain= False
+    for i in athletes["Name"]:
+         if i == nome:
+            athlete_id = athletes[athletes["Name"]==nome]["Athlete_id"]
+            athlete_id = int(athlete_id)
+    contagem = sport_events["Host Country"].str.contains(pais_jogo).sum()
+    if contagem > 0:
+        contain = True
+    if athlete_id != 0 and contain == True:
+        data_nasc = athletes[athletes["Athlete_id"] == athlete_id]["Date of Birth"].values[0]
+        ano_nasc = str(data_nasc[-4:])
+        ano_nasc = int(ano_nasc)
+        data_jogo = int(sport_events.loc[(sport_events["Athlete_id"] == athlete_id) &
+                                         (sport_events["Host Country"] == pais_jogo), ["Year"]].values[0])
+        ano_jogo = int (data_jogo)
+        idade = ano_jogo-ano_nasc
+    return idade
+
+#=============================================================================
+#=================== Respective Continents, from "nation" ====================
+#---- Caribbean Countries have been put into south_america
+asia = ["China", "Japan", "Malaysia", "South Korea"]
+africa = ["South Africa"]
+europe = ["Czech Republic", "Estonia", "France", "Germany", "Great Britain", "Greece", "Italy",
+          "Latvia", "Netherlands", "Poland", "Russia", "Slovakia", "Spain"]
+north_america = ["United States"]
+oceania = ["Australia", "New Zealand"]
+south_america = ["Barbados", "Colombia", "Cuba", "Trinidad and Tobago", "Venezuela"]
+
+#================= Assign Country to a Continent =====================
+def getContinent(country):
+    if country in asia:
+        continent = "Asia"
+    elif country in africa:
+        continent = "Africa"
+    elif country in europe:
+        continent = "Europe"
+    elif country in north_america:
+        continent = "North America"
+    elif country in oceania:
+        continent = "Oceania"
+    elif country in south_america:
+        continent = "South America"
+    else:
+        continent = None
+    return continent
+
+#===============================================================
+#================== Leitura de Ficheiros =======================
+#===============================================================
+#================= athletes.csv =====================
+athletes = pd.read_csv ("./csv_files/athletes.csv")
+
+#================= sport_events.csv =====================
+sport_events = pd.read_csv ("./csv_files/sport_events.csv")
+sport_events.drop(sport_events.tail(1).index,inplace=True) #remover o ano de 1896
+athletes.drop(athletes.tail(1).index,inplace=True)
+#===============================================================
+#============ Correcções à Base de Dados =======================
+#===============================================================
+#---- Mudar tipo de dados e Renomear Colunas
+sport_events.columns = ["Year","Athlete_id","Race_id","Rounds","Heat","Time","Average Km/H",
+                        "Rank","Result","Record","Host Country","Venue"]
+
+athletes.columns = ["Athlete_id","Name","Date of Birth","Nation","Sex"]
+
+sport_events["Time"] = sport_events["Time"].str.replace(',', '.')
+sport_events["Time"] = pd.to_numeric(sport_events["Time"],errors='coerce')
+
+sport_events["Average Km/H"] = sport_events["Average Km/H"].str.replace(',', '.')
+sport_events["Average Km/H"] = pd.to_numeric(sport_events["Average Km/H"],errors='coerce')
+
+sport_events["Rounds"] = sport_events["Rounds"].astype("category")
+sport_events["Record"] = sport_events["Record"].astype("category")
+
+#---- Verificações
+#print(athletes.dtypes)
+#print(sport_events.dtypes)
+
+#===============================================================
+#=================== Tabelas Descritivas =======================
+#===============================================================
+#================= Na´s (valores omissos) por coluna da df sport_events ===========
+valores_omissos_sport_events = sport_events.isna().sum()
+valores_omissos_sport_events = valores_omissos_sport_events.to_frame()
+valores_omissos_sport_events.columns=["Sum"]
+valores_omissos_sport_events = valores_omissos_sport_events[valores_omissos_sport_events["Sum"] > 0]
+saveImages("table", dfi, valores_omissos_sport_events)
+
+#================= Média, Moda, Mediana, etc =====================
+time_avg = sport_events[["Time","Average Km/H"]]
+summary_sport_events = time_avg.describe()
+saveImages("table",dfi, summary_sport_events)
+
+#================= Nrº Ciclistas por País =====================
+athlete_countries = athletes.groupby('Nation', as_index=False).size().sort_index(ascending=True)
+athlete_countries = athlete_countries.rename(columns={athlete_countries.keys()[0]: "Nation", athlete_countries.keys()[1]: "NºCiclistas"})
+athlete_countries = athlete_countries.sort_values(by=["NºCiclistas"])
+
+#---- Criar listas de atletas por país
+cols = athlete_countries.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+athlete_countries = athlete_countries[cols]
+saveImages("table",dfi, athlete_countries)
+
+#================= Nome dos Vencedores por Edição =====================
+df = pd.DataFrame(sport_events, columns = ["Athlete_id","Result","Time","Record"])
+
+df_2 = df[df["Result"] == "1"]
+df_2 = df_2.drop(columns=["Result"])
+
+lista=[]
+for i in range(df_2.shape[0]):
+    lista.append(1)
+array1 = np.array(lista)
+df_2["Result"] = array1
+#print(df_2)
+
+#---- Agregar Linhas de Colunas
+df_2 = df_2.groupby('Athlete_id', as_index=False).agg(
+    Victories=pd.NamedAgg(column='Result', aggfunc='sum'), 
+    Average_Time=pd.NamedAgg(column='Time', aggfunc='mean'),
+    Record=pd.NamedAgg(column='Record', aggfunc='first'))
+
+lista_vencedores=[]
+for j in df_2["Athlete_id"]:
+    for i in athletes["Athlete_id"]:
+        if i == j and athletes["Name"][i-1] not in lista_vencedores:
+            lista_vencedores.append(athletes["Name"][i-1])
+
+df_2.drop(df_2.columns[0], axis = 1, inplace = True)
+df_2.insert(0, "Name", lista_vencedores)
+saveImages("table",dfi, df_2)
+
+#===============================================================
+#======================== Gráficos =============================
+#===============================================================
+#================= Ciclistas por Continente =====================
+country = athletes["Nation"]
+#---- Add Continent column to dataframe
+athletes['Continent'] = country.apply(lambda x: getContinent(x))
+athletes["Continent"] = athletes["Continent"].astype('category')
+#---- Create Piechart
+athlete_continents = athletes.groupby("Continent", as_index=False).size().sort_index(ascending=False)
+athlete_continents = athlete_continents.rename(columns={athlete_continents.keys()[0]: "Continente", athlete_continents.keys()[1]: "NºCiclistas"})
+athlete_continents = athlete_continents.sort_values(by=["NºCiclistas"], ascending = True)
+cols = athlete_continents.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+athlete_continents = athlete_continents[cols]
+
+pie, ax = plt.subplots(figsize=[10,6])
+labels = []
+for i in athlete_continents["Continente"]:
+    labels.append(i)
+x_data = []
+for j in athlete_continents["NºCiclistas"]:
+    x_data.append(j)
+plt.title("Cyclists per Continent", fontsize=14)
+pie_chart = plt.pie(x=x_data, autopct="%.1f%%", explode=[0.05]*6, labels=labels, pctdistance=0.5)
+saveImages("graph",pie,None)
+
+#================= Média de Tempo por Ronda e Edição =====================
+avg_t_per_round_year = pd.DataFrame(data=sport_events.groupby(["Year","Rounds"], as_index=False)["Time"].mean())
+
+#---- Renomear e Agregar Rondas Similares
+avg_t_per_round_year = avg_t_per_round_year.replace("1/16 Finals Repechages", "16th-Finals Repechages")
+avg_t_per_round_year = avg_t_per_round_year.replace("1/8 Finals Repechages", "Eighth-Finals Repechages")
+avg_t_per_round_year = avg_t_per_round_year.replace(["Final for Gold - Race 1","Final for Gold - Race 2",
+                                                     "Final for Gold - Race 3"], "Final for Gold")
+avg_t_per_round_year = avg_t_per_round_year.replace(["Final for bronze - Race 1",
+                                                     "Final for bronze - Race 2","Final for bronze - Race 3"], "Final for Bronze")
+avg_t_per_round_year = avg_t_per_round_year.replace(["Quarter Final 1 - Race 1","Quarter Final 1 - Race 2",
+                                                     "Quarter Final 2 - Race 1","Quarter Final 2 - Race 2",
+                "Quarter Final 3 - Race 1","Quarter Final 3 - Race 2","Quarter Final 4 - Race 1",
+                "Quarter Final 4 - Race 2"], "Quarterfinals")
+avg_t_per_round_year = avg_t_per_round_year.replace(["Semifinals 1 - Race 1","Semifinals 1 - Race 2",
+                                                     "Semifinals 1 - Race 3","Semifinals 2 - Race 1",
+                "Semifinals 2 - Race 2","Semifinals 2 - Race 3"], "Semifinals")
+avg_t_per_round_year = avg_t_per_round_year.replace("Qualifying", "Qualifying")
+avg_t_per_round_year = avg_t_per_round_year.replace("Race for 5th-8th Places", "5-8th Place Race")
+avg_t_per_round_year = avg_t_per_round_year.replace("Race for 9th-12th Places", "9-12th Place Race")
+
+avg_t_per_round_year = avg_t_per_round_year.replace("1/16 Finals", "16th-Finals")
+avg_t_per_round_year = avg_t_per_round_year.replace("1/8 Finals", "Eighth-Finals")
+
+avg_t_per_round_year = avg_t_per_round_year.groupby(["Year","Rounds"],as_index=False).mean()
+
+#---- Line Plot setup - Order races from earlier to last round
+lg_avg_t_per_round_year = sns.catplot(x="Rounds", y="Time", hue="Year", 
+                                      kind="point", data = avg_t_per_round_year, 
+                                      order=['Qualifying','16th-Finals','16th-Finals Repechages',
+                                             '9-12th Place Race','Eighth-Finals',
+                                             'Eighth-Finals Repechages','5-8th Place Race',
+                                             'Quarterfinals', 'Semifinals', 'Final for Bronze',
+                                             'Final for Gold'])
+
+lg_avg_t_per_round_year.fig.set_figheight(10)
+lg_avg_t_per_round_year.fig.set_figwidth(20)
+lg_avg_t_per_round_year.set_yticklabels(size = 10)
+lg_avg_t_per_round_year.set_xticklabels(size = 8.25)
+saveImages("graph",lg_avg_t_per_round_year,None)
+
+#================= Média de km/H por Edição =====================
+avg_kmh_per_round_year = pd.DataFrame(data=sport_events.groupby(["Average Km/H","Year"], 
+                                                                as_index=False)["Average Km/H"].mean())
+avg_kmh_per_round_year = pd.DataFrame(data=sport_events.groupby("Year")["Average Km/H"].mean())
+avg_kmh_per_round_year["Year"] = avg_kmh_per_round_year.index
+#---- Line Plot setup
+lg_avg_kmh_per_round_year = sns.catplot(x="Year", y="Average Km/H", kind="point", data = avg_kmh_per_round_year)
+lg_avg_kmh_per_round_year.fig.set_figheight(10)
+lg_avg_kmh_per_round_year.fig.set_figwidth(20)
+
+lg_avg_kmh_per_round_year.set_yticklabels(size = 12.5).set_xticklabels(size = 12.5)
+saveImages("graph",lg_avg_kmh_per_round_year,None)
+
+#================= Média de Tempo por Edição =====================
+avg_time_per_round_year = pd.DataFrame(data=sport_events.groupby(["Time","Year"], 
+                                                                as_index=False)["Time"].mean())
+
+avg_time_per_round_year = pd.DataFrame(data=sport_events.groupby("Year")["Time"].mean())
+avg_time_per_round_year["Year"] = avg_time_per_round_year.index
+avg_time_per_round_year.astype("category")
+#---- Line Plot setup
+lg_avg_time_per_round_year = sns.catplot(x="Year", y="Time", kind="point", data = avg_time_per_round_year)
+
+lg_avg_time_per_round_year.fig.set_figheight(10)
+lg_avg_time_per_round_year.fig.set_figwidth(20)
+lg_avg_time_per_round_year.set_yticklabels(size = 12.5).set_xticklabels(size = 12.5)
+
+saveImages("graph",lg_avg_time_per_round_year,None)
+
+#================== Média de Idade por Edição =============================
+df_3 = pd.DataFrame(sport_events, columns = ["Host Country"])
+
+lista_nomes_ciclistas=[]
+for i in sport_events["Athlete_id"]: # i -> athlete_id
+    for j in athletes["Athlete_id"]:
+         if i == j:
+             lista_nomes_ciclistas.append(athletes["Name"][i-1])
+df_3["name"] = lista_nomes_ciclistas        
+df_3 = df_3.drop_duplicates()
+df_3 = df_3.reset_index(drop=True)
+lista_idades = []
+for i in range(len(df_3)):
+    lista_idades.append(obterIdade(df_3["name"][i],df_3["Host Country"][i]))
+df_3["Age"] = lista_idades
+mean_age_by_edition = df_3.groupby(["Host Country"], as_index=False)["Age"].mean()
+
+mean_age_by_edition = mean_age_by_edition.replace("Brazil", "2016")
+mean_age_by_edition = mean_age_by_edition.replace("United Kingdom", "2012")
+mean_age_by_edition = mean_age_by_edition.replace("China", "2008")
+mean_age_by_edition = mean_age_by_edition.replace("Greece", "2004")
+mean_age_by_edition = mean_age_by_edition.replace("Australia", "2000")
+mean_age_by_edition.rename(columns={"Host Country": "Olympic Edition"}, inplace=True)
+mean_age_by_edition["Age"] = mean_age_by_edition["Age"].round(3)
+mean_age_by_edition.astype("category")
+
+mean_age_by_edition_barplot = sns.catplot(x="Olympic Edition", y="Age", hue="Olympic Edition", 
+                                          palette="tab10", kind="bar", dodge=False, data=mean_age_by_edition,
+                                          order=["2000", "2004", "2008", "2012", "2016"])
+
+for i in mean_age_by_edition_barplot.ax.patches:
+    mean_age_by_edition_barplot.ax.annotate("%.2f" % i.get_height(), (i.get_x() + i.get_width() / 2.,
+                                            i.get_height()),ha='center', va='center',
+                                            fontsize=13, color='black', xytext=(0, 20),
+                                            textcoords='offset points')
+      
+mean_age_by_edition_barplot.set(ylim=(23.5, None))
+mean_age_by_edition_barplot.fig.set_figheight(12)
+mean_age_by_edition_barplot.fig.set_figwidth(15)
+saveImages("graph",mean_age_by_edition_barplot,None)
+
+#================= Média de Tempo por Corrida  =====================
+boxplot_one = sns.catplot(x="Time", y="Rounds", kind="box", data=sport_events)
+
+boxplot_one.fig.set_figheight(12)
+boxplot_one.fig.set_figwidth(25)
+
+saveImages("graph",boxplot_one,None)
+
+#===============================================================
+#======================== Tests ================================
+#===============================================================
+#================ Tempo para correr código  ====================
+end = time.time()
+formatted_float = "{:.2f}".format(end - start)
+print("\n Code took "+formatted_float+" seconds to run.")
+
+#================ Test Class  ====================
+class testFunctions(unittest.TestCase):
+
+    def test_obterIdade_with_valid_parameters(self):
+        self.assertEqual(obterIdade("Jason Kenny","Brazil"), 28)
+
+    def test_obterIdade_with_country_not_in_dataset(self):
+        self.assertEqual(obterIdade("Jason Kenny","Portugal"), None)
+
+    def test_obterIdade_with_athlete_not_in_dataset(self):
+        self.assertEqual(obterIdade("Max","Brazil"), None)
+
+    def test_obterIdade_with_both_parameters_not_in_dataset(self):
+        self.assertEqual(obterIdade("Max","Portugal"), None)
+
+    def test_getContinent_with_both_parameters_not_in_dataset(self):
+        self.assertEqual(getContinent("Russia"), "Europe")
+    
+    def test_getContinent_with_country_not_in_dataset(self):
+        self.assertEqual(getContinent("Portugal"), None)
+
+unittest.main(argv=['first-arg-is-ignored'], exit=False) #deve retornar ok!
